79e3f63f4444cf50f0dcbc2c32f8e47fdccdf0ab
TlMegalovania <tlmegalovania@qq.com>
2022-01-29 00:05:24 +0800

    refactor: minimum api

--- Middleware/TestMiddleware.cs
+++ Middleware/TestMiddleware.cs
@@ -1,22 +1,21 @@
-﻿using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
-using OneBot.CommandRoute.Models;
+﻿using OneBot.CommandRoute.Models;
 using OneBot.CommandRoute.Services;
 
 namespace OneBot.FrameworkDemo.Middleware;
 
-public class TestMiddleware: IOneBotMiddleware
+public class TestMiddleware : IOneBotMiddleware
 {
-    private ILogger<TestMiddleware> _logger;
+    private readonly ILogger<TestMiddleware> _logger;
 
     public TestMiddleware(ILogger<TestMiddleware> logger)
     {
         _logger = logger;
     }
-    
+
     public ValueTask Invoke(OneBotContext oneBotContext, OneBotRequestDelegate next)
     {
-        using (_logger.BeginScope("机器人开始处理东西了")) {
+        using (_logger.BeginScope("机器人开始处理东西了"))
+        {
             return next(oneBotContext);
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,20 +1,35 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using OneBot.CommandRoute.Mixin;
+using OneBot.CommandRoute.Models.VO;
+using OneBot.CommandRoute.Services;
+using OneBot.FrameworkDemo.Middleware;
+using OneBot.FrameworkDemo.Modules;
 
-namespace OneBot.FrameworkDemo
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+IConfiguration configuration = builder.Configuration;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+#region ConfigureServices
+// 配置机器人核心
+// 设置 OneBot 配置
+services.Configure<CQHttpServerConfigModel>(configuration.GetSection("CQHttpConfig"))
+    .ConfigureOneBot();
+
+// 添加中间件
+// 单例模式或原型模式都可以，问题不大。
+// services.AddSingleton<IOneBotMiddleware, TestMiddleware>();
+services.AddScoped<IOneBotMiddleware, TestMiddleware>()
+
+// 添加指令 / 事件
+// 推荐使用单例模式（而实际上框架代码也是当单例模式使用的）
+    .AddSingleton<IOneBotController, TestModule>();
+// 一行一行地将指令模块加进去
+#endregion
+
+var app = builder.Build();
+
+#region Configure
+// 再也不需要手动启动 Sora 服务了
+// 具体改动见 https://github.com/ParaParty/OneBot-CommandRoute-Template/issues/2
+#endregion
+
+app.Run();
